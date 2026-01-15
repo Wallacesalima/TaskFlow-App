b12a258df04d400054cfb66e17a88a85101e63f2
WALLACE SA LIMA <wallacealmeida9999@gmail.com>
2026-01-15 18:18:26 -0300

    estrutura inicial da lista de tarefas com checkbox.
    Logica de adição de tarefas 90% concluído.

--- js/app.js
+++ js/app.js
@@ -1,31 +1,51 @@
-const list = document.querySelector('.task-list')
 
-const checkbox = document.createElement('input')
-checkbox.type = 'checkbox';
-checkbox.name = 'input-checkbox'
-// checkbox.checked = true
-checkbox.classList.add('input-checkbox')
+document.addEventListener('DOMContentLoaded', () => {
+    const form = document.getElementById('task-form')
+    form.addEventListener('submit', (e) => {
+        e.preventDefault()
+        createTask()
+    })
 
-const label = document.createElement('label')
-label.classList.add('label-checkbox')
+})
+
+function createTask() {
+    const input = document.getElementById('input-add')
+    const taskList = document.getElementById('task-list')
+
+    const inputAdd = input.value.trim()
+    if (inputAdd === '') return
+
+    const li = document.createElement('li')
+    li.classList.add('task-item')
+
+    const label = document.createElement('label')
+    label.classList.add('label-checkbox')
+
+    const span = document.createElement('span')
+    span.classList.add('span-item')
+
+    li.appendChild(label)
+    span.textContent = inputAdd
+    li.appendChild(span)
+    input.value = ''
+    const checkbox = document.createElement('input')
+    checkbox.type = 'checkbox';
+    checkbox.name = 'input-checkbox'
+    checkbox.classList.add('input-checkbox')
+
+    label.appendChild(checkbox)
 
 
-const image = document.createElement('img')
-image.src = "assets/images/trash-2.svg" 
-image.classList.add('image-trash')
+    const button = document.createElement('button')
+    button.type = 'button'
 
-const li = document.createElement('li')
-li.textContent = 'teste'
+    const image = document.createElement('img')
+    image.src = "assets/images/trash-2.svg"
+    image.classList.add('image-trash')
 
-li.classList.add('task-item')
-const item = document.querySelector('.task-item')
+    button.appendChild(image)
 
-if (checkbox.checked === true) {
-    li.classList.add('input-checked')
+    li.appendChild(button)
+
+    taskList.appendChild(li)
 }
-
-list.append(label)
-label.appendChild(checkbox)
-list.append(li)
-list.appendChild(image)
-
