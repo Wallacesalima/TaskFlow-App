e7e911cbee0a506b3cf4144c9aff6629202afb82
WALLACE SA LIMA <wallacealmeida9999@gmail.com>
2026-01-16 13:55:35 -0300

    FUNÇÃO ADICIONAR E REMOVER COMPLETAS
    App já suposta adicionar e remover tarefas, estou adicionando a função de salvar no storage do navegador e carregar os dados quando abrir a página.

--- js/app.js
+++ js/app.js
@@ -1,16 +1,19 @@
 
 document.addEventListener('DOMContentLoaded', () => {
+    let contador = 0
     const form = document.getElementById('task-form')
     form.addEventListener('submit', (e) => {
         e.preventDefault()
-        createTask()
+        createTask(contador)
+        contador++
     })
 
 })
 
-function createTask() {
+function createTask(contador) {
     const input = document.getElementById('input-add')
     const taskList = document.getElementById('task-list')
+    let index = 'task-' + contador
 
     const inputAdd = input.value.trim()
     if (inputAdd === '') return
@@ -20,32 +23,66 @@ function createTask() {
 
     const label = document.createElement('label')
     label.classList.add('label-checkbox')
+    label.htmlFor = index
 
     const span = document.createElement('span')
     span.classList.add('span-item')
 
     li.appendChild(label)
     span.textContent = inputAdd
-    li.appendChild(span)
     input.value = ''
+
     const checkbox = document.createElement('input')
+
+    checkbox.addEventListener('change', () => {
+        saveTasks()
+    })
+
+    checkbox.id = index
     checkbox.type = 'checkbox';
     checkbox.name = 'input-checkbox'
     checkbox.classList.add('input-checkbox')
 
     label.appendChild(checkbox)
+    label.appendChild(span)
 
 
     const button = document.createElement('button')
     button.type = 'button'
+    button.ariaLabel = 'Deletar'
 
     const image = document.createElement('img')
     image.src = "assets/images/trash-2.svg"
     image.classList.add('image-trash')
+    image.title = 'trash/lixeira/deletar'
 
     button.appendChild(image)
 
     li.appendChild(button)
 
     taskList.appendChild(li)
+    saveTasks()
+
+    button.addEventListener('click', () => {
+        const li = button.parentElement
+        li.remove()
+        saveTasks()
+    })
+}
+
+function saveTasks() {
+    const tasks = []
+
+    document.querySelectorAll('.task-item').forEach(li => {
+        const text = li.querySelector('.span-item').textContent
+        const completed = li.querySelector('.input-checkbox').checked
+
+        tasks.push({text, completed})
+    })
+
+    localStorage.setItem('tasks', JSON.stringify(tasks))
+}
+
+function loadTasks() {
+    localStorage.getItem('tasks', JSON.parse())
 }
